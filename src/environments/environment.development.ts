a6c41b53a42a81b20e08f8bea83403eabb698e88
Angel Palomares <angel.palomares@gmail.com>
2025-05-07 14:40:11 +0200

    added the licenses, fix the name of the new PID

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,17 @@
+// Copyright 2025 Atos Spain S.A. All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 export const environment = {
     production: false,
 
@@ -5,7 +19,7 @@ export const environment = {
     USELF_AUTHENTIC_SOURCE_URL : "http://localhost:8888",
     USELF_KEYCLOAK_URL : "http://uself-keycloak.localhost",
 
-    USELF_PID: "eu.europa.ec.eudi.pid",
+    USELF_PID: "VerifiablePID",
 
     AUTHORIZATION_ENDPOINT: "/auth/authorize",
     INITIATE_OFFER_ENDPOINT: "/issuer/initiate-credential-offer",
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,17 @@
+// Copyright 2025 Atos Spain S.A. All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 export const environment = {
     production: true,
 
@@ -5,7 +19,7 @@ export const environment = {
     USELF_AUTHENTIC_SOURCE_URL : window['env' as any]['USELF_AUTHENTIC_SOURCE_URL' as any] || "http://uself-authentic-source.local",
     USELF_KEYCLOAK_URL : window['env' as any]['USELF_KEYCLOAK_URL' as any] || "http://uself-keycloak.local",
 
-    USELF_PID: window['env' as any]['USELF_PID' as any] || "eu.europa.ec.eudi.pid",
+    USELF_PID: window['env' as any]['USELF_PID' as any] || "VerifiablePID",
 
     AUTHORIZATION_ENDPOINT: "/auth/authorize",
     INITIATE_OFFER_ENDPOINT: "/issuer/initiate-credential-offer",
